a0a01d2320653f8bc2c1c5469c6b6d1839641430
luke <122482468+Lukew69@users.noreply.github.com>
2023-04-11 19:00:59 -0700

    Update HoneyProdProject.R

--- HoneyProdProject.R
+++ HoneyProdProject.R
@@ -22,8 +22,8 @@ cor(HoneyProd$year, HoneyProd$colonies_number)
 cor(HoneyProd$year, HoneyProd$production)
 cor(HoneyProd$year, HoneyProd$value_of_production)
 
-cor(HoneyProd$colonies_number, HoneyProd$production)#
-cor(HoneyProd$colonies_number, HoneyProd$value_of_production)#
+cor(HoneyProd$colonies_number, HoneyProd$production) #good
+cor(HoneyProd$colonies_number, HoneyProd$value_of_production) #good
 
 cor(HoneyProd$production, HoneyProd$value_of_production)
 
@@ -35,5 +35,35 @@ summary(USreg1)
 
 USreg2=lm(HoneyProd$colonies_number~HoneyProd$value_of_production )
 plot(HoneyProd$colonies_number, HoneyProd$value_of_production, xlab="Amount of Producing Colonies", ylab="Value (dollars)")
+abline(USreg2)
+summary(USreg2)
+
+#apply a natural log transformation
+#creating new columns in the data set for easier use later
+
+HoneyProd <- HoneyProd %>%
+  mutate(
+    colonies_number.ln = log(colonies_number)
+  )
+HoneyProd <- HoneyProd %>%
+  mutate(
+    production.ln = log(production)
+  )
+HoneyProd <- HoneyProd %>%
+  mutate(
+    value_of_production.ln = log(value_of_production)
+  )
+View(HoneyProd)
+
+#run them again with the transformation
+cor(HoneyProd$colonies_number.ln, HoneyProd$production.ln)
+USreg1=lm(HoneyProd$colonies_number.ln~HoneyProd$production.ln )
+plot(HoneyProd$colonies_number.ln, HoneyProd$production.ln, xlab="Amount of Producing Colonies", ylab="Honey Produced (pounds)")
+abline(USreg1)
+summary(USreg1)
+
+cor(HoneyProd$colonies_number.ln, HoneyProd$value_of_production.ln)
+USreg2=lm(HoneyProd$colonies_number.ln~HoneyProd$value_of_production.ln )
+plot(HoneyProd$colonies_number.ln, HoneyProd$value_of_production.ln, xlab="Amount of Producing Colonies", ylab="Value (dollars)")
 abline(USreg2)
 summary(USreg2)
